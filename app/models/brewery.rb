7fbac9b6619707e247a55df122ccd31e4b24a48e
Jarno Leppänen <jarno.leppanen@iki.fi>
2014-01-20 02:29:42 +0200

    brewery ratings

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,12 @@
 class Brewery < ActiveRecord::Base
-    has_many :beers, :dependent => :destroy
+  has_many :beers, :dependent => :destroy
+  has_many :ratings, :through => :beers
+
+  def average_rating
+    if ratings.empty?
+      nil
+    else
+      ratings.reduce(0) { |sum, r| sum + r.score } / ratings.count.to_f
+    end
+  end
 end
