1b50c4038bbffa2ae227d543fc615e9c0b6ed2d9
Jarno Leppänen <jarno.leppanen@iki.fi>
2014-01-20 02:14:12 +0200

    added possibility to destroy ratings

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Ratebeer::Application.routes.draw do
   root 'breweries#index'
   resources :beers
   resources :breweries
-  resources :ratings, :only => [:index, :new, :create]
+  resources :ratings, :only => [:index, :new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
